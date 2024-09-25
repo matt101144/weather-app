8d6774a1349b0babdb1869a4b335ccc2504b9332
matt101144 <matt.neville10@outlook.com>
2024-09-25 17:46:41 +0100

    declared the variables that are needed

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 const apiKey = "99feadb57e8d55cf23608fc968c9fd96";
+const weatherForm = document.querySelector(".weatherForm");
+const cityChoice = document.querySelector(".cityChoice");
+const weatherCard = document.querySelector(".weatherCard");
