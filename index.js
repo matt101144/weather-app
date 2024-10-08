a3a1afa76f10b3254ac676176dacd8ad84d8c274
matt101144 <matt.neville10@outlook.com>
2024-10-08 04:17:25 +0100

    updated js file  link

---
+++ index.js
@@ -0,0 +1,47 @@
+const apiKey = "99feadb57e8d55cf23608fc968c9fd96";
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const weatherCard = document.querySelector(".weatherCard");
+
+weatherForm.addEventListener("submit", async event => {
+
+    event.preventDefaultMethod();
+
+    const city = cityInput.value;
+
+    if(city){
+        try{
+            const weatherData = await getWeatherData(city);
+            displayWeatherInfo(weatherData);
+        }
+        catch(error){
+            console.error(error);
+            displayError(error);
+        }
+    }
+    else{
+        displayError("please enter a correct city");
+    }
+
+
+});
+
+async function getWeatherData(city){
+
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    const response = await fetch(apiUrl);
+
+    console.log(response);
+};
+
+function displayError(message){
+
+    const errorDisplay = document.createElement("p");
+    errorDisplay.textContent = message;
+    errorDisplay.classList.add("errorDisplay");
+
+    card.textContent = "";
+    card.style.display = "flex";
+    card.appendChild(errorDisplay);
+};
